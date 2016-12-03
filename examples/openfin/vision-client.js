0bee7e9b238361d3a3ba9ba4c4c0993dfd49c451
dsandor <david.sandor@gmail.com>
2016-12-03 14:58:30 -0600

    trying to get reconnects working.

--- examples/openfin/vision-client.js
+++ examples/openfin/vision-client.js
@@ -1,4 +1,16 @@
 window.vision = {
+  websocket: {},
+
+  uri: '',
+
+  connectionRetryMax: 0, // 0 = unlimited retries
+
+  connectionRetryIntervalMax: 10 * 1000, // maximum number of milliseconds to delay a retry attempt.
+
+  connectionRetryCount: 0,
+
+  connectionRetryInterval: 1000,
+
   init: (options) => {
     const ERROR_HOST_REQUIRED = 'Host is a required option.';
 
@@ -12,18 +24,16 @@ window.vision = {
     let host = options.host,
       port = options.port || 16999;
 
+    window.vision.uri = `ws://${host}:${port}/`;
+
     if (document && document.addEventListener) {
       document.addEventListener('DOMContentLoaded', function () {
         if (fin && fin.desktop) {
           fin.desktop.main(function () {
-            var wsUri = `ws://${host}:${port}/`;
-            websocket = new WebSocket(wsUri);
-            websocket.onopen = (evnt) => {
-              console.log('onopen - event:' + JSON.stringify(evnt));
-              window.vision.heartbeat(websocket);
-            };
-            websocket.onmessage = window.vision.messageHandler;
+            window.vision.connect();
           });
+        } else {
+          console.log('openfin environment not found. Not connecting to vision server.');
         }
       });
     }
@@ -31,7 +41,11 @@ window.vision = {
 
    heartbeat: (socket) => {
      setTimeout(() => {
-       socket.send(JSON.stringify({type: 'heartbeat', location: window.location, connectionId: window.vision.connectionId}));
+       socket.send(JSON.stringify({
+         type: 'heartbeat',
+         location: window.location,
+         connectionId: window.vision.connectionId
+       }));
        window.vision.heartbeat(socket);
      }, window.vision.options.heartbeatInterval || 1000);
 
@@ -45,6 +59,43 @@ window.vision = {
 
     if (message.type === 'handshake') {
       window.vision.connectionId = message.connectionId;
+    }
+  },
+
+  connect: (uri) => {
+    var wsUri = uri || window.vision.uri;
+
+    try {
+      window.vision.websocket = new WebSocket(wsUri);
+    } catch(err) {
+      console.log(`Failed connecting to vision server: ${wsUri}, retrying with back-off.`);
+
+      window.vision.connectionRetryCount++;
+
+      if (window.vision.connectionRetryMax > 0 && window.vision.connectionRetryMax > window.vision.connectionRetryCount) {
+        console.log(`Connection retries exhausted. Max: ${window.vision.connectionRetryMax}`);
+        return;
+      }
+
+      window.vision.connectionRetryInterval = Math.pow(2, window.vision.connectionRetryCount) * 1000;
+
+      if (window.vision.connectionRetryInterval > window.vision.connectionRetryIntervalMax) {
+        window.vision.connectionRetryInterval = window.vision.connectionRetryIntervalMax;
+      }
+
+      setTimeout(() => window.vi.connect(uri), window.vision.connectionRetryInterval);
+      console.log(`Retrying connection to server in ${window.vision.connectionRetryInterval/100} seconds.`);
+    }
+
+    if (!window.vision.websocket.onopen) {
+      window.vision.websocket.onopen = (evnt) => {
+        console.log('onopen - event:' + JSON.stringify(evnt));
+        window.vision.heartbeat(window.vision.websocket);
+      };
+    }
+
+    if (!window.vision.websocket.onmessage) {
+      window.vision.websocket.onmessage = window.vision.messageHandler;
     }
   }
 };
