2101b6132f12e72f2d666ecf4ee767a5e7849184
dsandor <david.sandor@gmail.com>
2016-12-08 21:24:03 -0600

    Adding web stuff.

--- api/lib/index.js
+++ api/lib/index.js
@@ -1,55 +1,14 @@
 'use strict';
 
-const { Server } = require('ws'),
-      wss        = new Server({ port: 16999 }),
-      uuid       = require('uuid');
-
-const connections = {};
-
-function sendMessage(socket, type, message) {
-  return new Promise((resolve, reject) => {
-    socket.send(JSON.stringify(Object.assign({ type }, message)), (err) => {
-      if (!err) return resolve();
-
-      console.log('got error from client [%s]: %s', socket.connectionId, err.message);
-
-      return reject(err);
-    });
-  });
-}
-
-function sendTicks(socket) {
-  setTimeout(() => {
-    sendMessage(socket, 'tick', { time: new Date() })
-      .then(() => sendTicks(socket))
-      .catch(() => {
-        console.log('removing client connect due to failure');
-        delete connections[socket.connectionId];
-      });
-  }, 1000);
-}
+const Collector = require('./collector'),
+      collector = new Collector({ autoStart: true, port: 16999 });
 
 function diagnostics() {
   setTimeout(() => {
-    console.log('## Connection count: %d', Object.keys(connections).length);
+    console.log('## Connection count: %d', Object.keys(collector.connections).length);
     diagnostics();
   }, 5000);
 }
-
-wss.on('connection', (ws) => {
-  const connectionId = uuid.v4();
-
-  ws.connectionId = connectionId; // eslint-disable-line no-param-reassign
-  connections[connectionId] = ws;
-
-  sendMessage(ws, 'handshake', { connectionId });
-
-  ws.on('message', (message) => {
-    console.log('[id:%s] received: %s', connectionId, message);
-  });
-
-  sendTicks(ws);
-});
 
 diagnostics();
 // TODO: ws.onDisconnect - remove the connection from the hashmap
