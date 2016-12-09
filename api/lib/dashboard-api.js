ef2ba0e768e54a5cd9ccf46484767eee84e678f8
dsandor <david.sandor@gmail.com>
2016-12-08 21:26:22 -0600

    material kit stuff

---
+++ api/lib/dashboard-api.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const uuid = require('uuid'),
+      { Server }  = require('ws');
+
+function sendMessage(socket, type, message) {
+  return new Promise((resolve, reject) => {
+    socket.send(JSON.stringify(Object.assign({ type }, message)), (err) => {
+      if (!err) return resolve();
+
+      console.log('got error from client [%s]: %s', socket.connectionId, err.message);
+
+      return reject(err);
+    });
+  });
+}
+
+function sendStats(socket) {
+  setTimeout(() => {
+    sendMessage(socket, 'stats', { time: new Date() })
+      .then(() => sendStats(socket))
+      .catch(() => {
+        console.log('removing client connect due to failure');
+        delete this.connections[socket.connectionId];
+      });
+  }, 1000);
+}
+
+class Collector {
+  constructor(options) {
+    options = options || {};
+    this.port = options.port || 16998;
+    this.connections = {}
+    this.wsServer = new Server({ port: this.port });
+
+    if (options.autoStart) this.initSocket();
+  }
+
+  initSocket() {
+    this.wsServer.on('connection', (ws) => {
+      const connectionId = uuid.v4();
+
+      ws.connectionId = connectionId; // eslint-disable-line no-param-reassign
+      this.connections[connectionId] = ws;
+
+      sendMessage(ws, 'handshake', { connectionId });
+
+      ws.on('message', (message) => {
+        console.log('[id:%s] received: %s', connectionId, message);
+      });
+
+      sendStats(ws);
+    });
+  }
+}
+
+module.exports = Collector;
